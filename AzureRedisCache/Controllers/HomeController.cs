99f5f0434cc2e29b173057a22fe0d173e3772ee8
nirzaf <mfmfazrin1986@gmail.com>
2022-06-14 13:37:36 +0530

    Initial commit

---
+++ AzureRedisCache/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace AzureRedisCache.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDistributedCache _cache;
+
+        public HomeController(IDistributedCache cache)
+        {
+            this._cache = cache;
+        }
+        public IActionResult Index()
+        {
+           string test = _cache.GetString("Test") ?? "";
+            if (string.IsNullOrEmpty( test))
+            {
+                _cache.SetString("Test", "Tested");
+                test = _cache.GetString("Test") ?? "";
+            }
+            ViewData["Test"] = test;
+            return View();
+        }
+
+    }
+}
